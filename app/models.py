85f4d4106edc225fc8e2746c3257f9bc6bc575fe
Anastasia <goncharovaanastasia53@gmail.com>
2026-06-30 10:30:51 +0700

    creation of models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название категории")
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+class Application(models.Model):
+    STATUS_CHOICES = [
+        ('new', 'Новая'),
+        ('in-progress', 'Принято в работу'),
+        ('completed', 'Выполнено'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь")
+    title = models.CharField(max_length=100, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
+    image = models.ImageField(upload_to='plans/', verbose_name="Фото помещения или план")
+    design_image = models.ImageField(upload_to='designs/', blank=True, null=True, verbose_name="Готовый дизайн")
+    comment = models.TextField(blank=True, null=True, verbose_name="Комментарий администратора")
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new', verbose_name="Статус")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Временная метка")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Заявка"
+        verbose_name_plural = "Заявки"
+        ordering = ['-created_at']
